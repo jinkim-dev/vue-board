610d4916cfaa6c099feef51ec55fb19b11fb7e03
MINJI KIM <rlaalswl627@gmail.com>
2020-03-09 23:09:41 +0900

    Init: vue-board project

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        writer: "헤르만 헤세",
+        title: "데미안",
+        content: "1"
+    },
+    {
+        writer: '루이자 메이 올컷',
+        title: "작은 아씨들",
+        content: "2"
+    },
+    {
+        writer: '이도우',
+        title: "날씨가 좋으면 찾아가겠어요",
+        content: "3"
+    }
+]
