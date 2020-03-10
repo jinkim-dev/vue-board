84b4268f0dcedf70a2aea74b6a984177eb7bf856
MINJI KIM <rlaalswl627@gmail.com>
2020-03-10 22:16:05 +0900

    Added: CRUD board component 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Read from '@/components/Read'
+import Create from '@/components/Create'
+import Detail from '@/components/Detail'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Read',
+      component: Read
+    },
+    {
+      path: '/create/:contentId?',
+      name: 'Create',
+      component: Create
+    },
+    {
+      path: '/detail/:contentId',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
